9761f3b82f1f9eb3e77e3f241d6e90a6770768e8
Elaa Affes <elaa.affes@supcom.tn>
2026-05-13 01:47:34 +0100

    Update cesar.h

--- cesar.h
+++ cesar.h
@@ -6,16 +6,10 @@
 #include <string.h>
 #include <ctype.h>
 
-// --- Prototypes des fonctions ---
-// (C'est la liste de ce que votre "librairie" sait faire)
-
-// Crypte ou décrypte un seul caractère
 char cesar_car(char c, int k);
 
-// Crypte toute une phrase (modifie la chaine directement)
 void cesar_crypte_mot(char *mot, int k);
 
-// Décrypte toute une phrase
 void cesar_decrypte_mot(char *mot, int k);
 
 #endif
